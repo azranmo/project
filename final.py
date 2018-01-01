c4e4100764b0dd2f1cd33e4042e96a9144932f6a
azranmo <moranazran11@gmail.com>
2018-01-01 16:09:55 +0200

    change gui 2

--- final.py
+++ final.py
@@ -81,6 +81,7 @@ class project:
 
     # The function init the gui
     def __init__(self, master):
+
         self.master = master
         self.master.title("out project")
         self.labelDir = Label(master, text="touches Path")
