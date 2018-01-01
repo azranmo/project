62ee36827824f6dab2ddd00ac9fd95153982b89a
azranmo <moranazran11@gmail.com>
2018-01-01 15:57:18 +0200

    change gui

--- final.py
+++ final.py
@@ -64,8 +64,8 @@ class Model:
 
 
     def build(self,filePath):
-        df = self.load_csv(filePath)
-        users = self.devide(df)
+        dfTouches = self.load_csv(filePath)
+        users = self.devide(dfTouches)
 
 class project:
     model = Model()
@@ -76,21 +76,27 @@ class project:
         self.var.set(file)
 
     def build(self):
-        filePath = self.var.get()
-        self.model.build(filePath)
+        touchesPath = self.var.get()
+        questPath = self.var2.get()
+        self.model.build(touchesPath,questPath)
 
     # The function init the gui
     def __init__(self, master):
         self.master = master
         self.master.title("out project")
-        self.labelDir = Label(master, text="file Path")
+        self.labelDir = Label(master, text="touches Path")
         self.var = StringVar()
         dirname = Entry(master, textvariable=self.var)
+        self.labelDir2 = Label(master, text="quest Path")
+        self.var2 = StringVar()
+        dirname2 = Entry(master, textvariable=self.var2)
         self.browse_button = Button(master, text="Browse", command=lambda: self.browse())  # button browse
         self.build_button = Button(master, text="Build", command=lambda: self.build())  # button build
         # self.classify_button = Button(master, text="Classify", command=lambda: self.classify())
         self.labelDir.grid(row=0, column=0, sticky=W)
+        self.labelDir.grid(row=1, column=0, sticky=W)
         dirname.grid(row=0, column=1, columnspan=2, sticky=W + E)
+        dirname2.grid(row=1, column=1, columnspan=2, sticky=W + E)
         self.browse_button.grid(row=0, column=3)
         self.build_button.grid(row=2, column=1)
         # self.classify_button.grid(row=3, column=1)
